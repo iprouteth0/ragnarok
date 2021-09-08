551adee0d75b43c574a27603eca22e03f6e732ca
iprouteth0 <jmrandall05@gmail.com>
2021-09-08 17:44:30 -0500

    undelegate function written but untested

--- undelegate.sh
+++ undelegate.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-mkdir delegate
-dialog --backtitle "Delegate menu" --title "Delegate to validator form" \
---form "\npopulate delegate form" 15 90 7 \
+mkdir undelegate
+dialog --backtitle "Undelegate menu" --title "Undelegate from validator form" \
+--form "\npopulate undelegate form" 15 90 7 \
 "Wallet addeess:" 1 1 "" 1 25 70 120  \
 "validator:" 2 1 "" 2 25 70 120  \
 "amount:" 3 1 "" 3 25 70 120 \
-"chain-id:" 4 1 "" 4 25 70 120 > delegate/out.tmp \
+"chain-id:" 4 1 "" 4 25 70 120 > undelegate/out.tmp \
 2>&1 >/dev/tty
 # Start retrieving each line from temp file 1 by one with sed and declare variables as inputs
-export ADDRESS=`sed -n 1p delegate/out.tmp`
-export VALIDATOR=`sed -n 2p delegate/out.tmp`
-export AMOUNT=`sed -n 3p delegate/out.tmp`
-export CHAIN=`sed -n 4p delegate/out.tmp`
-docker exec -it node bandd tx staking delegate $VALIDATOR $AMOUNT --from $ADDRESS --chain-id $CHAIN --gas auto
+export ADDRESS=`sed -n 1p undelegate/out.tmp`
+export VALIDATOR=`sed -n 2p undelegate/out.tmp`
+export AMOUNT=`sed -n 3p undelegate/out.tmp`
+export CHAIN=`sed -n 4p undelegate/out.tmp`
+docker exec -it node bandd tx staking unbond $VALIDATOR $AMOUNT --from $ADDRESS --chain-id $CHAIN --gas auto
 echo "press enter to continue"
 read
 # remove temporary file created
-rm -f delegate/out.tmp
+rm -f undelegate/out.tmp
 #Write to output file the result
 #echo $CODENAME , $MENUNUMBER , $TREEDIR , >> testfile
 
