60870322994ba86704e796830ca0ccef9dd76532
iprouteth0 <jmrandall05@gmail.com>
2021-09-18 20:12:59 -0500

    query mint functions completed

--- query/qmint.sh
+++ query/qmint.sh
@@ -1,23 +1,132 @@
 #!/bin/bash
+# Copyright (C) 2021 iprouteth0 software
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
 
-mkdir tmp
-dialog --backtitle "Delegate menu" --title "Delegate to validator form" \
---form "\npopulate delegate form" 15 90 7 \
-"Wallet addeess:" 1 1 "" 1 25 70 120  \
-"validator:" 2 1 "" 2 25 70 120  \
-"amount:" 3 1 "" 3 25 70 120 \
-"chain-id:" 4 1 "" 4 25 70 120 > tmp/delegate.tmp \
-2>&1 >/dev/tty
+export CHAIN=odin-testnet-baldur
 
-# Start retrieving each line from temp file 1 by one with sed and declare variables as inputs
-export ADDRESS=`sed -n 1p tmp/delegate.tmp`
-export VALIDATOR=`sed -n 2p tmp/delegate.tmp`
-export AMOUNT=`sed -n 3p tmp/delegate.tmp`
-export CHAIN=`sed -n 4p tmp/delegate.tmp`
+#functions
+annual-provisions() {
+dialog --title "minting annaual-provisions" --msgbox "$(docker exec -it node bandd query mint annual-provisions  --chain-id $CHAIN)" 40 90
+}
 
-docker exec -it node bandd tx staking delegate $VALIDATOR $AMOUNT --from $ADDRESS --chain-id $CHAIN --gas auto
-echo "press enter to continue"
-read
+eth-integration-address() {
+dialog --title "mint eth-integration-address" --msgbox "$(docker exec -it node bandd query mint eth-integration-address  --chain-id $CHAIN)" 40 90
+}
 
-# remove temporary file created
-rm -f tmp/delegate.tmp
+inflation() {
+dialog --title "minting inflation" --msgbox "$(docker exec -it node bandd query mint inflation --chain-id $CHAIN)" 40 90
+}
+
+params() {
+dialog --title "minting params" --msgbox "$(docker exec -it node bandd query mint params --chain-id $CHAIN)" 40 90
+}
+
+treasury-pool() {
+dialog --title "minting treasury-pool" --msgbox "$(docker exec -it node bandd query mint trrasury-pool --chain-id $CHAIN)" 40 90
+}
+
+#section1
+
+###############
+
+HEIGHT=30
+WIDTH=50
+CHOICE_HEIGHT=8
+BACKTITLE="RAGNAROK"
+TITLE="Query Menu"
+MENU="Choose one of the following options:"
+OPTIONS=(
+1 "Query minting annual-provisions"
+2 "Query minting eth-integration-address"
+3 "Query minting inflation"
+4 "query minting params"
+5 "query minting treasury-pool"
+0 "Go Back to Main"
+)
+while [[ ! $CHOICE = 0 ]]; do
+    CHOICE=$(dialog --clear \
+                    --backtitle "$BACKTITLE" \
+                    --title "$TITLE" \
+                    --menu "$MENU" \
+                    $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                    "${OPTIONS[@]}" \
+                    2>&1 >/dev/tty)
+
+case $CHOICE in
+1 )
+  # Motorola ginna build menu
+  clear
+  BEGIN=$(date +%s)
+  annual-provisions
+  END=$(date +%s)
+  clear
+;;
+#############################################################
+
+2 )
+  # Xiaomi Olivewood build menu
+  clear
+  BEGIN=$(date +%s)
+  eth-integration-address
+  END=$(date +%s)
+  clear
+;;
+###############################################################
+
+3 )
+  # query bank total
+  clear
+  BEGIN=$(date +%s)
+  inflation
+  END=$(date +%s)
+  clear
+;;
+###############################################################
+
+4 )
+  # Xiaomi Olivewood build menu
+  clear
+  BEGIN=$(date +%s)
+  params
+  END=$(date +%s)
+  clear
+;;
+###############################################################
+
+5 )
+  # query bank total
+  clear
+  BEGIN=$(date +%s)
+  treasury-pool
+  END=$(date +%s)
+  clear
+;;
+###############################################################
+# section4
+#############################################################
+0 ) 
+  # Return To Main Menu
+  ./query/qmenu.sh
+;; 
+* ) clear
+    echo "Wrong Choice AssFace, 1-3 or 0 to Return to Main Menu"
+    sleep 1
+    echo " "
+    echo "4"
+    sleep 1
+    echo "3"
+    sleep 1
+    echo "2"
+    sleep 1
+    echo "1"
+    sleep 1
+    clear
+    esac
+done
+###########################################################
+
