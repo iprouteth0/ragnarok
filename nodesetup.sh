8b1a2fe2d9b106f4700160b62ea8e323f39369f0
Iprouteth0 <jmrandall05@gmail.com>
2021-09-22 17:17:26 -0500

    Added before & after backup
    Added backup of initial iptables rules before modifications as well as backup after modifications

--- nodesetup.sh
+++ nodesetup.sh
@@ -10,11 +10,13 @@ sleep 15
 ./export.sh
 mkdir ~/odin/
 cp -R config/ ~/odin/
+sudo iptables-save > ~/initial_iptables_backup.txt
 export IPADDRESS=$(docker inspect node | grep IPAddress | grep 172 | cut -d"\"" -f4)
 export SOCKET="$(docker inspect node | grep IPAddress | grep 172 | cut -d"\"" -f4):26656"
 export SOCKET2="$(docker inspect node | grep IPAddress | grep 172 | cut -d"\"" -f4):26657"
 sudo iptables -t nat -A DOCKER -p tcp --dport 26656 -j DNAT --to-destination $SOCKET && sudo iptables -t nat -A POSTROUTING -j MASQUERADE -p tcp --source $IPADDRESS --destination 26656 && sudo iptables -A DOCKER -j ACCEPT -p tcp --destination $IPADDRESS --dport 26656
 sudo iptables -t nat -A DOCKER -p tcp --dport 26657 -j DNAT --to-destination $SOCKET2 && sudo iptables -t nat -A POSTROUTING -j MASQUERADE -p tcp --source $IPADDRESS --destination 26657 && sudo iptables -A DOCKER -j ACCEPT -p tcp --destination $IPADDRESS --dport 26657
+sudo iptables-save > ~/26656_26657_opened.txt
 cd ../..
 sleep 20
 echo "alias odin='docker exec -it node bandd'" >> ~/.bashrc && alias odin='docker exec -it node bandd'
