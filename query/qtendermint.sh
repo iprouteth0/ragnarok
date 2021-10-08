f4ac365c25140a838ef7ad208101054176871ac5
Iprouteth0 <jmrandall05@gmail.com>
2021-10-08 17:03:04 -0500

    Initial script
    .

--- query/qtendermint.sh
+++ query/qtendermint.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 
-mkdir tmp
-dialog --backtitle "Delegate menu" --title "Delegate to validator form" \
---form "\npopulate delegate form" 15 90 7 \
-"Wallet addeess:" 1 1 "" 1 25 70 120  \
-"validator:" 2 1 "" 2 25 70 120  \
-"amount:" 3 1 "" 3 25 70 120 \
-"chain-id:" 4 1 "" 4 25 70 120 > tmp/delegate.tmp \
-2>&1 >/dev/tty
 
-# Start retrieving each line from temp file 1 by one with sed and declare variables as inputs
-export ADDRESS=`sed -n 1p tmp/delegate.tmp`
-export VALIDATOR=`sed -n 2p tmp/delegate.tmp`
-export AMOUNT=`sed -n 3p tmp/delegate.tmp`
-export CHAIN=`sed -n 4p tmp/delegate.tmp`
-
-docker exec -it node bandd tx staking delegate $VALIDATOR $AMOUNT --from $ADDRESS --chain-id $CHAIN --gas auto
-echo "press enter to continue"
-read
-
-# remove temporary file created
-rm -f tmp/delegate.tmp
+dialog --title "Query tendermint-validator-set" --msgbox "$(docker exec -it node bandd query tendermint-validator-set --chain-id $CHAIN)"
